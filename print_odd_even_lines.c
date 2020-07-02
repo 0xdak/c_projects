aac453c3050bb211c1fbd18c71561ae13e550a37
Ali Dak <alidak583@gmail.com>
2020-07-02 18:50:17 +0300

    geeksforgeeks ex

---
+++ print_odd_even_lines.c
@@ -0,0 +1,27 @@
+//the task is to print all the odd line content of the file first
+//then print all the even line content
+//this is a "geeksforgeeks" example
+
+#include<stdio.h>
+
+int main(void){
+	FILE *fp;
+	char data[50]= "";
+	fp = fopen("file.txt", "r");
+	int i = 1;
+	while(fgets(data,50,fp) != NULL){
+		if((i % 2) == 0) printf("%s",data);
+		i++;
+	}
+	i = 1;
+	fclose(fp);
+	printf("\n");
+	fp = fopen("file.txt", "r");
+	while(fgets(data,50,fp) != NULL){
+		if((i % 2) == 1) printf("%s",data);
+		i++;
+	}
+	
+	fclose(fp);
+	return 0;
+}
